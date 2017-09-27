f17f4bfbaf7c68e25e1f373667a5e79be46d0542
wolf1994722 <herlmercedes@gmail.com>
2017-09-27 20:29:31 +0200

    Add CLI Options & Parser

---
+++ src/Dangl.AVA.Examples/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace Dangl.AVA.Examples
+{
+    public class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Relative or absolute path to a GAEB file")]
+        public string InputFilePath { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Relative or absolute path to the output file")]
+        public string OutputFilePath { get; set; }
+
+        [Option('k', "keepPrices", Required = false, Default = true, HelpText = "Keeps prices in the output")]
+        public bool KeepPrices { get; set; }
+
+        [Option('t', "target", Required = true, HelpText = "The transformation target")]
+        public Target Target { get; set; }
+    }
+}
---
+++ src/Dangl.AVA.Examples/OptionsParser.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+
+namespace Dangl.AVA.Examples
+{
+    public class OptionsParser
+    {
+        public OptionsParser(string[] commandLineArguments)
+        {
+            _commandLineArguments = commandLineArguments;
+            ParseOptions();
+        }
+
+        private readonly string[] _commandLineArguments;
+        public bool IsValid { get; private set; }
+        public Options Result { get; private set; }
+
+        private void ParseOptions()
+        {
+            var parsedOptions = Parser.Default.ParseArguments<Options>(_commandLineArguments);
+            if (parsedOptions is Parsed<Options> parserResult)
+            {
+                IsValid = true;
+                Result = parserResult.Value;
+            }
+        }
+    }
+}
